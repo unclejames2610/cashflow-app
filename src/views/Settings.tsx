0b2f2740a755fd77f24273f8e86783f63d14dfd6
unclejames2610 <udealaebuka@gmail.com>
2024-05-27 17:41:55 +0100

    feat:enabled entering of income

--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -3,15 +3,34 @@ import BasicButton from "@/components/BasicButton";
 import SettingsCard from "@/components/settingsComponents/SettingsCard";
 import SubHeader from "@/components/settingsComponents/SubHeader";
 import { FaToggleOff } from "react-icons/fa6";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ChangeEmail from "@/components/settingsComponents/ChangeEmail";
 import ChangePassword from "@/components/settingsComponents/ChangePasssword";
+import EnterIncome from "@/components/settingsComponents/EnterIncome";
+import { useRouter } from "next/navigation";
 
 const Settings = () => {
   const [changeEmail, setChangeEmail] = useState(false);
   const [changePassword, setChangePassword] = useState(false);
   const [currentEmail, setCurrentEmail] = useState("");
   const [success, setSuccess] = useState(false);
+  const [enterIncome, setEnterIncome] = useState(false);
+  const router = useRouter();
+
+  const logout = () => {
+    router.push("/");
+  };
+
+  useEffect(() => {
+    if (
+      changeEmail === true ||
+      success === true ||
+      changePassword === true ||
+      enterIncome === true
+    ) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [changeEmail, success, changePassword, enterIncome]);
   return (
     <div className="mx-auto flex flex-col min-h-screen bg-background gap-6 relative">
       {changeEmail && (
@@ -24,6 +43,21 @@ const Settings = () => {
             currentEmail={currentEmail}
             changeEmail={changeEmail}
             setChangeEmail={setChangeEmail}
+            setSuccess={setSuccess}
+            success={success}
+          />
+        </div>
+      )}
+
+      {enterIncome && (
+        <div className="w-full h-full absolute bg-black/40 z-10"></div>
+      )}
+
+      {enterIncome && (
+        <div className="absolute z-20 inset-0 flex mt-4">
+          <EnterIncome
+            enterIncomeActive={enterIncome}
+            setEnterIncomeActive={setEnterIncome}
             setSuccess={setSuccess}
             success={success}
           />
@@ -84,12 +118,16 @@ const Settings = () => {
       {/* date and time */}
       <div className="flex flex-col gap-5 px-4">
         {/* sub header */}
-        <SubHeader text="Date And Time" />
+        <SubHeader text="Income" />
 
         <div className="flex justify-between gap-5 items-center">
-          <p className="text-xs md:text-sm">Set Time Zone To Location</p>
+          <p className="text-xs md:text-sm">Set Income</p>
 
-          <FaToggleOff className="text-4xl text-primary-green" />
+          <BasicButton
+            notFull={true}
+            text="Enter Income"
+            onClick={() => setEnterIncome(true)}
+          />
         </div>
       </div>
 
@@ -104,7 +142,7 @@ const Settings = () => {
             subText="log out of all other active sessions besides this one"
           />
 
-          <BasicButton notFull={true} text="Logout" />
+          <BasicButton notFull={true} text="Logout" onClick={logout} />
         </div>
 
         <div className="flex items-center justify-between gap-6">
